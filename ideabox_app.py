95c19d8a91bcd97b13d83e282d1a1f5363e99cc9
fortune <fortune_ik@yahoo.com>
2016-02-26 06:26:32 +0100

    merged to master for heroku deployment

--- ideabox_app.py
+++ ideabox_app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request,\
- 				  redirect, url_for, session, flash, g
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask.ext.sqlalchemy import SQLAlchemy 
 from functools import wraps
-import sqlite3
+#import sqlite3
+
 
 # create the application object
 app = Flask(__name__)
@@ -9,7 +10,13 @@ app = Flask(__name__)
 #session key
 app.secret_key = "my God is alive"
 
-app.database = "./members.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///posts.db" 
+
+
+# create the sqlachemy object
+db = SQLAlchemy(app)
+
+from models import *
 
 # login required decorator
 def login_required(f):
@@ -31,36 +38,38 @@ def welcome():
 # handles the signup page
 @app.route("/signup", methods=['GET', 'POST'])
 def signup():
-	if request.method == "POST":
-		g.db = connect_db()
-		username = request.form['username']
-		email = request.form['email']
-		password = request.form['password']
 	return render_template('signup.html', title="Signup")
+
+
 
 # handles the login page
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+
+	print 'route hit'
 	error_msg = None
+	#print request.method
 	if request.method == 'POST':
-		if request.form['username'] != 'admin' \
-						or request.form['password'] != 'admin':
-			error = 'Invalid credentials. Please try again.'
-		else:
-			session['logged_in'] = True
-			flash("You're logged in")
-			return redirect(url_for('home'))
+		user = User.query.get(request.form['email'])
+	 	print user
+	
+		# if request.form['username'] != 'admin' \
+		# 	or request.form['password'] != 'admin':
+		# 	error = 'Invalid login'
+		# else:
+		# 	session['logged_in'] = True
+		# 	flash("login successful")
+		# 	return redirect(url_for('home'))
+
 	return render_template('login.html', error_msg=error_msg, title="Please login")
 
 # handles the home page
 @app.route('/')
 @login_required
 def home():
-	g.db = connect_db()
-	cur = g.db.execute('select * from ideas')
-	ideas = [dict(title=row[0], description=row[1]) for row in cur.fetchall()]
-	g.db.close()
-	return render_template('index.html', ideas=ideas, title="Ideabox | Home")
+	posts = db.session.query(IdeaPost).all()
+	return render_template('index.html', posts=posts, title="Ideabox | Home")
+
 
 # handles the post making page
 @app.route('/post')
@@ -68,32 +77,37 @@ def home():
 def post():
 	return render_template('post.html', title="Share an Idea")
 
+
 # handles the profile 
 @app.route('/profile')
 @login_required
 def profile():
 	return render_template('profile.html', title="Profile")
 
+
+
 # handles user logouts
 @app.route('/logout')
 @login_required
 def logout():
 	session.pop('logged_in', None)
+	flash('You are logged out')
 	return redirect(url_for('welcome'))
+
 
 # this maps the contact us page
 @app.route('/contact')
 def contact():
 	return render_template('contact.html', title='Contact')
 
+
 # maps the about us page
 @app.route('/about')
 def about():
 	return render_template('about.html', title='About')
 
-# initialization of db connection
-def connect_db():
-	return sqlite3.connect(app.database)
+"""def connect_db():
+	return sqlite3.connect('posts.db')"""
 
 if __name__ == "__main__":
 	app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,44 @@
+from ideabox_app import db
+
+class IdeaPost(db.Model):
+
+	__tablename__ = "posts"
+
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String, nullable=False)
+	description = db.Column(db.String, nullable=False)
+
+	def __init__(self, title, description):
+		self.title = title
+		self.description = description
+
+	def __repr__(self):
+		return "{}-{}".format(self.title, self.description)
+
+
+	__tablename__ = "posts"
+
+
+class User(db.Model):
+	
+    __tablename__ = 'user'
+
+    email = db.Column(db.String, primary_key=True)
+    password = db.Column(db.String)
+    authenticated = db.Column(db.Boolean, default=False)
+
+    def is_active(self):
+        """True, as all users are active."""
+        return True
+
+    def get_id(self):
+        """Return the email address to satisfy Flask-Login's requirements."""
+        return self.email
+
+    def is_authenticated(self):
+        """Return True if the user is authenticated."""
+        return self.authenticated
+
+    def is_anonymous(self):
+        """False, as anonymous users aren't supported."""
+        return False
